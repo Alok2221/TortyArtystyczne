79c13f3fdc3fc8427bf72dd10a7320dc61238e03
Aleks <aleksskela591@gmail.com>
2022-04-07 00:12:30 +0200

    add cake service and cake serviceImpl

---
+++ src/main/java/pl/zaczarowaneslodkosci/service/CakeServiceImpl.java
@@ -0,0 +1,48 @@
+package pl.zaczarowaneslodkosci.service;
+
+import org.springframework.stereotype.Service;
+import pl.zaczarowaneslodkosci.dto.CakeDtoRequest;
+import pl.zaczarowaneslodkosci.dto.CakeDtoResponse;
+import pl.zaczarowaneslodkosci.entity.CakeEntity;
+import pl.zaczarowaneslodkosci.repository.CakeRepository;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static org.springframework.beans.BeanUtils.copyProperties;
+
+@Service
+public class CakeServiceImpl implements CakeService {
+
+    private final CakeRepository cakeRepository;
+
+    public CakeServiceImpl(CakeRepository cakeRepository) {
+        this.cakeRepository = cakeRepository;
+    }
+
+    @Override
+    public CakeDtoResponse get(Long id) {
+        return new CakeDtoResponse(cakeRepository.getOrThrow(id));
+    }
+
+    @Override
+    public CakeDtoResponse add(CakeDtoRequest cakeDtoRequest) {
+        CakeEntity cakeEntity = new CakeEntity();
+        copyProperties(cakeDtoRequest, cakeEntity);
+        CakeEntity newCake = cakeRepository.save(cakeEntity);
+        return new CakeDtoResponse(newCake);
+    }
+
+    @Override
+    public void delete(Long id) {
+        cakeRepository.deleteById(id);
+    }
+
+    @Override
+    public List<CakeDtoResponse> search(String name) {
+        return cakeRepository.getByName(name)
+                .stream()
+                .map(CakeDtoResponse::new)
+                .collect(toList());
+    }
+}
