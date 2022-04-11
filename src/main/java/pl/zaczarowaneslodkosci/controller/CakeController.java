17f0cb489c613d878a2e6e319a8c0a64694b3566
Aleks <aleksskela591@gmail.com>
2022-04-11 21:33:58 +0200

    controller

--- src/main/java/pl/zaczarowaneslodkosci/controller/CakeController.java
+++ src/main/java/pl/zaczarowaneslodkosci/controller/CakeController.java
@@ -1,38 +1,15 @@
 package pl.zaczarowaneslodkosci.controller;
 
-import org.springframework.web.bind.annotation.*;
-import pl.zaczarowaneslodkosci.dto.CakeDtoRequest;
-import pl.zaczarowaneslodkosci.dto.CakeDtoResponse;
-import pl.zaczarowaneslodkosci.service.CakeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
-@RestController
+@Controller
 public class CakeController {
 
-    private final CakeService cakeService;
-
-    public CakeController(CakeService cakeService) {
-        this.cakeService = cakeService;
-    }
-
-    @GetMapping("/{id}")
-    public CakeDtoResponse get(@PathVariable Long id) {
-        return cakeService.get(id);
-    }
-
-    @PostMapping
-    public CakeDtoResponse add(@RequestBody CakeDtoRequest cakeDtoRequest) {
-        return cakeService.add(cakeDtoRequest);
-    }
-
-    @GetMapping
-    public List<CakeDtoResponse> search(String name) {
-        return cakeService.search(name);
-    }
-
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-        cakeService.delete(id);
+    @GetMapping("/start")
+    public String goTest(Model model) {
+        model.addAttribute("title", "My Title");
+        return "start";
     }
 }
